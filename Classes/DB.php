4b45f1348a8e71a866d01074340197a3500f43cb
genjo <insidia93@gmail.com>
2017-06-26 11:38:26 +0300

    Создали метод получения данных из бд

--- Classes/DB.php
+++ Classes/DB.php
@@ -16,4 +16,11 @@ class DB
         $this->dbh = new PDO('mysql:dbname=php13;dbhost=localhost','root','death9393');
         $this->dbh->exec('SET CHARSET utf8');
     }
+
+    public function getAll($sql,$params=[])
+    {
+        $sth = $this->dbh->prepare($sql);
+        $sth->execute($params);
+        return $sth->fetchAll(PDO::FETCH_CLASS);
+    }
 }
