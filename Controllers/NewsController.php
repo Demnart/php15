d3b3d91ca9fd062d6f5f2887c5faa64701f3beca
genjo <insidia93@gmail.com>
2017-06-26 12:17:47 +0300

    Создали наш контроллер

---
+++ Controllers/NewsController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class NewsController
+{
+
+    public function actionAll()
+    {
+        $items = NewsModel::findAll();
+        $view = new View();
+        $view->items = $items;
+        $view->display('news/all.php');
+    }
+
+    public function actionOne()
+    {
+        $id = $_GET['id'];
+        $item = NewsModel::findOneByPK($id);
+        $view = new View();
+        $view->item = $item;
+        $view->display('news/one.php');
+    }
+}
