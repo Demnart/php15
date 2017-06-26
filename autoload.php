020d65d2ecc717104c7df6014e304e33a2977e8c
genjo <insidia93@gmail.com>
2017-06-26 11:17:08 +0300

    Самостоятельная работа

---
+++ autoload.php
@@ -0,0 +1,21 @@
+<?php
+
+function __autoload($class)
+{
+    if (file_exists(__DIR__ . '/Classes/' . $class . '.php'))
+    {
+        require __DIR__ . '/Classes/' . $class . '.php';
+    }
+    else if(file_exists(__DIR__ . '/Controllers/' . $class . '.php'))
+    {
+        require __DIR__ . '/Controllers/' . $class . '.php';
+    }
+    else if(file_exists(__DIR__ . '/Models/' . $class . '.php'))
+    {
+        require __DIR__ . '/Models/' . $class . '.php';
+    }
+    else if(file_exists(__DIR__ . '/Views/' . $class . '.php'))
+    {
+        require __DIR__ . '/Views/' . $class . '.php';
+    }
+}
