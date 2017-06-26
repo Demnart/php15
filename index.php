a32780dc9d07bad79a1e23715671a8c1b53a3cfa
genjo <insidia93@gmail.com>
2017-06-26 13:24:50 +0300

    Создали фронт контроллер

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 require __DIR__ . '/autoload.php';
 
+$contr = isset($_GET['contr']) ? $_GET['contr'] : 'News';
+$act = isset($_GET['act']) ? $_GET['act'] : 'All';
 
+$controllerClassName = $contr . 'Controller';
+
+$controller = new $controllerClassName();
+$method = 'action' . $act;
+$controller->$method();
